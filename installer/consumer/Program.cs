9510406b481a06dcf1ea35bedf67fed3b6182d8d
Daniil Skotnikov <dskotnikov@icloud.com>
2023-01-05 16:31:58 +0300

    feat(installer): add installer

---
+++ installer/consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Installer.Utils;
+
+var expectedID = "";
+try
+{
+    expectedID = File.ReadAllText("license.txt");
+}
+catch
+{
+    Console.WriteLine("License not found");
+    Environment.Exit(1);
+}
+var actualID = IOPlatformSerialNumberProvider.Provide();
+if (expectedID == actualID)
+{
+    Console.WriteLine("License confirmed. Hello world!");
+}
+else
+{
+    Console.WriteLine("Lisense confirmation failed");
+    Environment.Exit(1);
+}
